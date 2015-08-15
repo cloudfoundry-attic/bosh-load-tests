a4802f09c1776779c6fae1049c538d7e82a80fdc
Maria Shaldibina <mshaldibina@pivotal.io>
2015-08-14 17:44:11 -0700

    Move everything to gopath
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -6,3 +6,6 @@ export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
 export GOPATH=$(pwd)/gopath
 cd gopath/src/github.com/cloudfoundry-incubator/bosh-load-tests
 go run main.go ci/concourse-config.json
+
+
+go run src/github.com/cloudfoundry-incubator/bosh-load-tests/main.go ci/concourse-config.json
