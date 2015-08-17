dfce146fa1bfd435a94d8d8d7bbac0cd4011e1bf
Kam Leung <kleung@pivotal.io>
2015-08-17 10:03:30 -0700

    Fix concourse relative paths
    Signed-off-by: Shatarupa Nandi <snandi@pivotal.io>
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -3,9 +3,30 @@
 set -e
 
 export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
-export GOPATH=$(pwd)/gopath
-cd gopath/src/github.com/cloudfoundry-incubator/bosh-load-tests
-go run main.go ci/concourse-config.json
 
+echo 'Starting DB...'
+su postgres -c '
+  export PATH=/usr/lib/postgresql/9.4/bin:$PATH
+  export PGDATA=/tmp/postgres
+  export PGLOGS=/tmp/log/postgres
+  mkdir -p $PGDATA
+  mkdir -p $PGLOGS
+  initdb -U postgres -D $PGDATA
+  pg_ctl start -l $PGLOGS/server.log
+'
 
-go run src/github.com/cloudfoundry-incubator/bosh-load-tests/main.go ci/concourse-config.json
+source /etc/profile.d/chruby.sh
+chruby 2.1.6
+
+echo 'Installing dependencies...'
+(
+	cd bosh-src
+	bundle install --local
+	bundle exec rake spec:integration:install_dependencies
+)	
+
+echo 'Running tests...'
+
+export GOPATH=$(realpath bosh-load-tests)
+
+go run bosh-load-tests/src/github.com/cloudfoundry-incubator/bosh-load-tests/main.go bosh-load-tests/ci/concourse-config.json
